c2851f438cfde82e215e4a95930ad24f84b23b88
Callum Boylan <2boylc17@solent.ac.uk>
2023-12-12 16:21:04 +0000

    Task 7 complete

---
+++ Task_7.py
@@ -0,0 +1,41 @@
+class Queue:
+    def __init__(self, capacity):
+        self.internalArray = [None] * capacity
+        self.front = 0
+        self.back = 0
+        self.capacity = capacity
+
+    def add(self, item):
+        if self.internalArray[self.back] is not None:
+            print("Queue is full")
+        else:
+            self.internalArray[self.back] = item
+            self.back = self.back + 1
+            if self.back == self.capacity:
+                self.back = 0
+            print(f"Added {item} to queue")
+        pass
+
+    def remove(self):
+        if self.internalArray[self.front] is not None:
+            removed = self.internalArray[self.front]
+            self.internalArray[self.front] = None
+            self.front = self.front + 1
+            if self.front == self.capacity:
+                self.front = 0
+            return print(f"{removed} has been removed from the queue")
+        else:
+            return print("Queue is empty")
+        pass
+
+    def __str__(self):
+        return self.internalArray.__str__()
+
+def run_task7a(queries):
+    print("What is your query?")
+    new_query = str(input()).capitalize()
+    queries.add(new_query)
+
+
+def run_task7b(queries):
+    queries.remove()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Task_2 import run_task2
 from Task_3 import run_task3
 from Task_4 import run_task4
 from Task_5 import run_task5
+from Task_7 import Queue, run_task7a, run_task7b
 import csv
 
 points_of_interest = []
@@ -11,6 +12,7 @@ with open("data.csv", "r") as file:
     next(csv_reader)
     for values in csv_reader:
         points_of_interest.append(values)
+queries = Queue(10)
 
 
 def run_task():
@@ -20,7 +22,9 @@ def run_task():
           f"[3] - Display all places to stay\n"
           f"[4] - Search for a type of place to stay\n"
           f"[5] - Make a booking for a place to stay\n"
-          f"[6] - Exit")
+          f"[6] - Make an Enquiry\n"
+          f"[7] - Answer an Enquiry\n"
+          f"[8] - Exit")
     num = input()
     if num == '1':
         run_task1(points_of_interest)
@@ -43,12 +47,20 @@ def run_task():
         input("(Press Enter)")
         run_task()
     if num == '6':
+        run_task7a(queries)
+        input("(Press Enter)")
+        run_task()
+    if num == '7':
+        run_task7b(queries)
+        input("(Press Enter)")
+        run_task()
+    if num == '8':
         print("Thank you")
         print(points_of_interest)
-        with open("data.csv", "w") as file:
-            file.write("name,type,location,availability\n")
+        with open("data.csv", "w") as new_file:
+            new_file.write("name,type,location,availability\n")
             for num in range(len(points_of_interest)):
-                file.write(f"{points_of_interest[num][0]},{points_of_interest[num][1]},{points_of_interest[num][2]},{points_of_interest[num][3]}\n")
+                new_file.write(f"{points_of_interest[num][0]},{points_of_interest[num][1]},{points_of_interest[num][2]},{points_of_interest[num][3]}\n")
         exit()
     else:
         print("Input not recognised")
